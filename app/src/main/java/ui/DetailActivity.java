33da6c46484d128423f9fa90644bf497e59a7147
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2017-05-18 15:40:26 +0700

    Detail Scaffolded

---
+++ app/src/main/java/ui/DetailActivity.java
@@ -0,0 +1,42 @@
+package ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
+
+import com.apec.dispatcher.R;
+
+import base.BaseActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ui.courierdetail.CourierDetailFragment;
+
+public class DetailActivity extends BaseActivity {
+
+    @BindView(R.id.container)
+    FrameLayout viewContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+        if (savedInstanceState == null) {
+            Fragment newFragment = new CourierDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, newFragment).commit();
+        }
+
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_detail;
+    }
+
+
+    @Override
+    public void setTitle(CharSequence title) {
+        this.setTitle("Co");
+    }
+}
---
+++ app/src/main/java/ui/courierdetail/CourierDetailFragment.java
@@ -0,0 +1,28 @@
+package ui.courierdetail;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.apec.dispatcher.R;
+
+
+public class CourierDetailFragment extends Fragment {
+
+
+    public CourierDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_courier_detail, container, false);
+
+    }
+
+}
